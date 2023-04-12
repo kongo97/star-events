fb353aa882958950099bbd35a2e3cdad8ec7ca97
scostantini <scostantini@ppresstoday.com>
2023-04-12 17:58:17 +0200

    aggiunti componenti primari

---
+++ src/resources/views/footer.blade.php
@@ -0,0 +1,37 @@
+<!-- footer begin -->
+<footer class="style-3">
+    <div class="container-fluid pl30 pr30">
+        <div class="row align-items-middle">
+            <div class="col-md-3">
+                <img src="images-artist/logo.png" class="logo-small" alt=""><br>
+            </div>
+
+            <div class="col-md-6">
+                &copy; Copyright 2023 - Archi Interior Design Template by <span class="id-color">Designesia</span>
+            </div>
+
+            <div class="col-md-3 text-right">
+                <div class="social-icons">
+                    <a href="#"><i class="fa fa-facebook fa-lg"></i></a>
+                    <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
+                    <a href="#"><i class="fa fa-rss fa-lg"></i></a>
+                    <a href="#"><i class="fa fa-google-plus fa-lg"></i></a>
+                    <a href="#"><i class="fa fa-skype fa-lg"></i></a>
+                    <a href="#"><i class="fa fa-dribbble fa-lg"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <a href="#" id="back-to-top"></a>
+</footer>
+<!-- footer close -->
+<!-- Javascript Files
+================================================== -->
+<script src="js/plugins.js"></script>
+<script src="js/designesia.js"></script>	
+
+<!-- RS5.0 Core JS Files -->
+<script src="revolution/js/jquery.themepunch.tools.min.js?rev=5.0"></script>
+<script src="revolution/js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
---
+++ src/resources/views/galleria.blade.php
@@ -0,0 +1,70 @@
+
+
+        <!-- content begin -->
+        <div id="content" class="no-top no-bottom">
+
+        	<div class="v-text">
+        	    <a href="#">facebook</a>
+        	    <a href="#">twitter</a>
+        	    <a href="#">instagram</a>
+        	</div>
+			
+			<section id="section-text">
+                <div class="container">
+                    <div class="col-md-12">
+                    	<div class="de_large-portfolio">
+                    		<a class="d_inner" href="12_single-artist.html">
+                    			<h2 class="ultra-big wow fadeInUp" data-wow-delay=".2s">Twin Den Spire</h2>
+                    			<div class="d_image">
+                    				<img src="images-artist/portfolio/1.jpg" alt="" class="img-fluid wow fadeInUp">
+                                    <span>Architecture in New York, 2022</span>
+                    			</div>
+                    		</a>
+                    	</div>
+
+                    	<div class="de_large-portfolio">
+                    		<a class="d_inner" href="12_single-artist.html">
+                    			<h2 class="ultra-big wow fadeInUp" data-wow-delay=".2s">Circular Tower</h2>
+                    			<div class="d_image">
+                    				<img src="images-artist/portfolio/2.jpg" alt="" class="img-fluid wow fadeInUp">
+                                    <span>Architecture in Washington, 2021</span>
+                    			</div>
+                    		</a>
+                    	</div>
+
+                    	<div class="de_large-portfolio">
+                    		<a class="d_inner" href="12_single-artist.html">
+                    			<h2 class="ultra-big wow fadeInUp" data-wow-delay=".2s">Elemental Spire</h2>
+                    			<div class="d_image">
+                    				<img src="images-artist/portfolio/3.jpg" alt="" class="img-fluid wow fadeInUp">
+                                    <span>Architecture in California, 2020</span>
+                    			</div>
+                    		</a>
+                    	</div>
+
+                    	<div class="de_large-portfolio">
+                    		<a class="d_inner" href="12_single-artist.html">
+                    			<h2 class="ultra-big wow fadeInUp" data-wow-delay=".2s">Pasture Tower</h2>
+                    			<div class="d_image">
+                    				<img src="images-artist/portfolio/4.jpg" alt="" class="img-fluid wow fadeInUp">
+                                    <span>Architecture in Alabama, 2019</span>
+                    			</div>
+                    		</a>
+                    	</div>
+
+                    	<div class="de_large-portfolio mb60">
+                    		<a class="d_inner" href="12_single-artist.html">
+                    			<h2 class="ultra-big wow fadeInUp" data-wow-delay=".2s">Ridge Spire</h2>
+                    			<div class="d_image">
+                    				<img src="images-artist/portfolio/5.jpg" alt="" class="img-fluid wow fadeInUp">
+                                    <span>Architecture in Florida, 2018</span>
+                    			</div>
+                    		</a>
+                    	</div>
+                    </div>
+                </div>
+            </section>
+
+
+            
+        </div>
