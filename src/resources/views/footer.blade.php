e5c96c9bc295e8fa6cf28e09f4a568402fe30da9
scostantini <scostantini@ppresstoday.com>
2023-04-20 18:00:09 +0200

    delete items

--- src/resources/views/footer.blade.php
+++ src/resources/views/footer.blade.php
@@ -7,18 +7,7 @@
             </div>
 
             <div class="col-md-6">
-                &copy; Copyright 2023 - Archi Interior Design Template by <span class="id-color">Designesia</span>
-            </div>
-
-            <div class="col-md-3 text-right">
-                <div class="social-icons">
-                    <a href="#"><i class="fa fa-facebook fa-lg"></i></a>
-                    <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
-                    <a href="#"><i class="fa fa-rss fa-lg"></i></a>
-                    <a href="#"><i class="fa fa-google-plus fa-lg"></i></a>
-                    <a href="#"><i class="fa fa-skype fa-lg"></i></a>
-                    <a href="#"><i class="fa fa-dribbble fa-lg"></i></a>
-                </div>
+                
             </div>
         </div>
     </div>
